1de640dd5316257bf1a26d0ad92a932ed70f5adf
ama <ama@it-keeper.ru>
2018-07-02 09:48:15 +0400

    lesson 1 (rdoc, rubycop)

--- lesson.rb
+++ lesson.rb
@@ -1,60 +1,83 @@
-
 class Host
-    NAME = "Host Class"
-    def initialize(ip="127.0.0.1", hostname ="")
-        @ip, @hostname =  ip, hostname
-    end
-    def getInfo
-        @info = "'"+@hostname.to_s+"'='"+@ip.to_s+"'" 
-        return @info
-    end
+  ##
+  # initialize Host by user data, or default data
+  #
+  # default data sets host's ip address 127.0.0.1 without host name
+  # data option - host ip address, host name
+  #
+  def initialize(ipaddress = '127.0.0.1', hostname = '')
+    @ipaddress, @hostname = ipaddress, hostname
+  end
+
+  ##
+  # return  compliance host ip address and host name (  example > 'server1'='192.168.1.100')
+  def getinfo
+    @info = "'" + @hostname.to_s + "'='" + @ipaddress.to_s + "'"
+    @info
+  end
 end
 
 class Computer < Host
-    NAME = "Computer Class"
-    #@@user = Array.new
-    def initialize(ip="127.0.0.1", dnsname ="", service="pc")
-        super(ip, dnsname)
-        @service  =  service
-    end
-    def getInfo
-        super
-        @info = @info + " Servise: ("+@service.to_s+")" 
-        return @info
-    end
+  ##
+  # initialize Computer by user data, or default data
+  #
+  # default data sets Computer's ip address 127.0.0.1 without host name and default mode pc (personal computer)
+  # data option - Computer ip address, Computer name,Computer mode
+  #
+  def initialize(ipaddress = '127.0.0.1', dnsname = '', service = 'pc')
+    super(ipaddress, dnsname)
+    @service = service
+  end
+
+  ##
+  # return  Computer host ip address and host name and mode  (  example > 'server1'='192.168.1.100' Servise: (RDP Server 1C 8.2)  )
+  def getinfo
+    super
+    @info = @info + ' Servise: (' + @service.to_s + ')'
+    @info
+  end
 end
 
 require 'ipaddr'
 class Network
-    NAME = "Network Class"
-    def initialize(address="",mask="")
-        @netStr = address.to_s+"/"+mask.to_s
-        @net = IPAddr.new(@netStr)
-    end
+  ##
+  # initialize Network by user data, or default data
+  #
+  # default data sets Network's ip address192.168.0.0 and network mask 24
+  # data option - network ip address, network mask
+  #
+  def initialize(address = '192.168.0.0', mask = '24')
+    @net_str = address.to_s + '/' + mask.to_s
+    @net = IPAddr.new(@net_str)
+  end
 end
 
 class Customer
-    NAME = "Customer Class"
-    #attr_accessor : user_id
-    #user_id = 0
-    def initialize(usrName="Иван", usrSName="Ивановов", usrMName="Иванович", usrOccupation = "разнорабочий")
-      @usrName, @usrSName, @usrMName, @usrOccupation = usrName, usrSName, usrMName, usrOccupation ;
-      #user_id += 1
-    end
-    def profession
-        puts @usrOccupation
-    end
-    def fio
-        puts @usrName.to_s + " " + @usrSName.to_s + " " + @usrMName.to_s
-    end
+  ##
+  # initialize Customer by user data, or default data
+  #
+  # default data - 'Иван Иванов Иванович разнорабочий'
+  # data option - user name, user surname, user midle name, user occupation
+  #
+  def initialize(usr_name = 'Иван', usr_sname = 'Иванов', usr_mname = 'Иванович', usr_occupation = 'разнорабочий')
+    @usr_name, @usr_sname, @usr_mname, @usr_occupation = usr_name, usr_sname, usr_mname, usr_occupation
+  end
+
+  ##
+  # return user occupation ( example > разнорабочий )
+  #
+  def profession
+    @usr_occupation
+  end
+
+  ##
+  # Return user's name surname middle name  ( example > Иванов Иван Иванович )
+  #
+  def fio
+    @usr_sname.to_s + ' ' + @usr_name.to_s + ' ' + @usr_mname.to_s
+  end
 end
 
-#usr = Customer.new()
-#usr.fio
-#usr.profession
-
-#comp = Computer.new("192.168.0.1", "amapc")
-#puts comp.getInfo 
-
-hst = Host.new("192.168.0.1", "amapc")
-puts hst.getInfo 
+usr = Customer.new
+puts usr.fio
+puts usr.profession
